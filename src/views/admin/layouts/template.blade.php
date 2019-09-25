c3d5abcc4913a3b4832a9da105b2c065176f16ae
Jamal Al-Matari <jalmatari@gmail.com>
2019-09-25 21:20:50 +0300

    Enhanced: elfinder

--- src/views/admin/layouts/template.blade.php
+++ src/views/admin/layouts/template.blade.php
@@ -14,11 +14,10 @@
 
 
     <script type="text/javascript">
-        var _globalObj = '{!!   json_encode(['_token' => csrf_token()]) !!}';
-        _globalObj = JSON.parse(_globalObj);
+            {!! 'var _globalObj = '.  json_encode(['_token' => csrf_token()]).';' !!}
         var _globalUrl = '{{url('/')}}';
     </script>
-<!-- jQuery 2.1.4 -->
+    <!-- jQuery 2.1.4 -->
     <script src="{{url('/')}}/jalmatari/plugins/jQuery/jQuery-2.1.4.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
     <script src="{{url('/')}}/jalmatari/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
--- src/views/helpers/ckeditor.blade.php
+++ src/views/helpers/ckeditor.blade.php
@@ -1,13 +1,14 @@
 <div id="fileManagerPanel"></div>
-<link rel="stylesheet" href="{{url('/')}}/jalmatari/plugins/jalmatari/css/elfinder.min.css">
-<link href="{{url('/jalmatari/plugins/jQueryUI/jquery-ui.min.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('jalmatari/plugins/jalmatari/css/elfinder.min.css')}}">
+<link rel="stylesheet" href="{{asset('jalmatari/plugins/jalmatari/css/theme.css')}}">
+<link href="{{asset('jalmatari/plugins/jQueryUI/jquery-ui.min.css')}}" rel="stylesheet">
 
 
-<script src="{{url('/')}}/jalmatari/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<script src="{{url('/jalmatari/plugins/jQueryUI/jquery-ui.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('jalmatari/plugins/jQuery/jQuery-2.1.4.min.js')}}"></script>
+<script src="{{asset('jalmatari/plugins/jQueryUI/jquery-ui.min.js')}}" type="text/javascript"></script>
 
-<script type="text/javascript" src="{{url('/')}}/jalmatari/plugins/jalmatari/js/elfinder.min.js"></script>
-<script type="text/javascript" src="{{url('/')}}/jalmatari/plugins/jalmatari/js/i18n/elfinder.ar.js"></script>
+<script type="text/javascript" src="{{asset('jalmatari/plugins/jalmatari/js/elfinder.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('jalmatari/plugins/jalmatari/js/i18n/elfinder.ar.js')}}"></script>
 <script>
     $(function () {
         $('#fileManagerPanel').elfinder({
@@ -16,13 +17,13 @@
             width: '100%',
             height: '100%',
             dateFormat: 'Y-m-d',
-            resizable : false,
+            resizable: false,
             {!! 'customData: '.  json_encode(['_token' => csrf_token()]).',' !!}
             getFileCallback: function (file, fm) {
-                window.opener.CKEDITOR.tools.callFunction((function() {
-                    var reParam = new RegExp('(?:[\?&]|&amp;)CKEditorFuncNum=([^&]+)', 'i') ;
-                    var match = window.location.search.match(reParam) ;
-                    return (match && match.length > 1) ? match[1] : '' ;
+                window.opener.CKEDITOR.tools.callFunction((function () {
+                    var reParam = new RegExp('(?:[\?&]|&amp;)CKEditorFuncNum=([^&]+)', 'i');
+                    var match = window.location.search.match(reParam);
+                    return (match && match.length > 1) ? match[1] : '';
                 })(), fm.convAbsUrl(file.url));
                 fm.destroy();
                 window.close();
