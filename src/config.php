2d7d8be7b8004c5e41dbc48d23953c5dd41023b6
Jamal Al-Matari <jalmatari@gmail.com>
2019-09-28 22:08:37 +0300

    Add jalmatari.api.allowable_tables config

--- src/config.php
+++ src/config.php
@@ -5,4 +5,7 @@ return [
     'permissions' => [
         'log_with_user' => 'إمكانية تسجيل الدخول كأي مستخدم في النظام',
     ],
+    'api'         => [
+        'allowable_tables' => [ "users", "sync" ]
+    ]
 ];
