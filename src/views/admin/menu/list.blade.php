7e31a31afd588748dee967bbc4a4835059f4532f
Jamal Al-Matari <jalmatari@gmail.com>
2019-10-31 12:12:47 +0300

    Enhanced: Menus page list order function

--- src/views/admin/menu/list.blade.php
+++ src/views/admin/menu/list.blade.php
@@ -18,7 +18,13 @@
             $(".sortable").sortable({
                 items: "tbody tr:not(.not-sortable)",
                 placeholder: "ui-state-highlight",
+                start:function (event, ui) {
+                    $('.ui-state-highlight').css('height',ui.item[0].style.height);
+                    if($('.ui-state-highlight').length==1)
+                        $('.ui-state-highlight').after('<tr class="ui-state-highlight hidden"></tr>')
+                },
                 stop: function (event, ui) {
+                    $('.ui-state-highlight.hidden').remove();
                     var id = $(ui.item[0]).find('[name="id[]"]').val();//ui.item[0].childNodes[0].innerText.trim();
                     api({
                         ac: 'orderMenus',
@@ -33,4 +39,9 @@
             oTable.draw();
         }
     </script>
+    <style>
+        .ui-sortable-helper{
+            display: table;
+        }
+    </style>
 @stop
