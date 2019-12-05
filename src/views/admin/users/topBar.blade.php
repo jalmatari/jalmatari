50a23f940eb751e8698ec814ff10a6c49a6a7706
Jamal Al-Matari <jalmatari@gmail.com>
2019-12-06 01:46:12 +0300

    Deleted: Static Add New User Btn

--- src/views/admin/users/topBar.blade.php
+++
@@ -1,5 +0,0 @@
-<a href="{{route_('admin.users.add')}}" class="btn btn-primary" >
-        <i class="fa fa-plus-square"></i>
-        <span>إضافة مستخدم جديد</span>
-        <strong></strong>
-    </a>
